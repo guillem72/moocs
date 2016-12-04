42da02ac5d12febb67b53635e9daf253dedfc51f
Guillem Lluch Moll <guillem72@gmail.com>
2016-12-04 09:46:32 +0100

    License information added to each source file.

--- src/main/java/glluch/com/moocs/GenerateTestResults.java
+++ src/main/java/glluch/com/moocs/GenerateTestResults.java
@@ -28,8 +28,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.SerializationUtils;
 
 /**
  * A class for do some files for tests.
